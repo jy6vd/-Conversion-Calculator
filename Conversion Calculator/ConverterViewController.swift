d8149cab51eea432e90ce8cc6a4d7eddd70f3cc7
Jonathan Yee <jy6vd@mail.missouri.edu>
2019-05-09 14:35:44 -0500

    need to add more

--- Conversion Calculator/ConverterViewController.swift
+++ Conversion Calculator/ConverterViewController.swift
@@ -15,6 +15,7 @@ class ConverterViewController: UIViewController {
         var inputVar: String
     }
     
+    var number: Float = 0.0
     @IBOutlet weak var outputDisplay: UITextField!
     @IBOutlet weak var inputDisplay: UITextField!
     
@@ -22,13 +23,19 @@ class ConverterViewController: UIViewController {
     var converters: [converter] = [converter(label: "fahrenheit to celcius", outputVar: "°C", inputVar: "°F"), converter(label: "celcius to fahrenheit", outputVar: "°F", inputVar: "°C"), converter(label: "miles to kilometers", outputVar: "km", inputVar: "mi"), converter(label: "kilometers to miles", outputVar: "mi", inputVar: "km")]
     
     
-    var currentString: String = "°F"
-    var outputString: String = "°C"
+   var currentString: String = " "
+    //"°F"
+   var outputString: String = " "
+    //"°C"
     var total = 0.0, outputVar = "°C", inputVar = "°F"
     override func viewDidLoad() {
         super.viewDidLoad()
-        outputDisplay.text = "°C"
-        inputDisplay.text = "°F"
+        //outputDisplay.text = "°C"
+        self.outputString = self.outputString + self.outputVar
+        self.outputDisplay.text = self.outputString
+        self.currentString = self.currentString + self.inputVar
+        self.inputDisplay.text = self.currentString
+        //inputDisplay.text = "°F"
         // Do any additional setup after loading the view.
     }
     @IBAction func conversionAlert(_ sender: AnyObject) {
@@ -53,12 +60,49 @@ class ConverterViewController: UIViewController {
     @IBAction func number(_ sender: UIButton) {
         currentString = currentString + Int(sender.tag - 1).description
         inputDisplay.text = currentString + inputVar
+        
+        if(!currentString.isEmpty){
+            if(currentString.hasPrefix("-")){
+                //currentString.removeLast(2)
+                switch (inputVar){
+                case "°F":
+                    number = -1*((Float(currentString) ?? 0.0)-(Float(32)) * (5/9))
+                case "°C":
+                    number = -1*(((Float(currentString) ?? 0.0)*(Float(5/9))) + 32)
+                case "mi":
+                    number = -1*((Float(currentString) ?? 0.0)/0.62137)
+                case "km":
+                    number = -1*((Float(currentString) ?? 0.0)*0.62137)
+                default:
+                    break
+                }
+            }else{
+                //currentString.removeLast(2)
+                switch (inputVar){
+                case "°F":
+                    
+                    number = ((Float(currentString) ?? 0.0)-(Float(32)) * (5/9))
+                case "°C":
+                    number = (Float(currentString) ?? 0.0)*(Float(5/9)) + 32
+                case "mi":
+                    number = (Float(currentString) ?? 0.0)/0.62137
+                case "km":
+                    number = (Float(currentString) ?? 0.0)*0.62137
+                default:
+                    break
+                }
+            }
+        }
+        outputDisplay.text = "\(number)" + outputVar
+        
     }
     @IBAction func misc(_ sender: UIButton) {
         switch(sender.tag){
         case 1:
             currentString = ""
             inputDisplay.text = currentString + inputVar
+            number = 0.0
+            outputDisplay.text = " " + outputVar
         case 2:
             if(currentString.contains("-")){
                 currentString.removeFirst()
